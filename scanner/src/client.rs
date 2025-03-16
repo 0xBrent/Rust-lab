d0c87777d702c3cf0a5181ed0d7fea2bd1d2da97
Brent <brentm623@gmail.com>
2025-03-16 02:11:49 -0700

    server function defined

--- scanner/src/client.rs
+++ scanner/src/client.rs
@@ -1,5 +1,7 @@
-use std::net::TcpStream;
+use std::{io::{stdin, Read, Write}, net::TcpStream};
 
-fn main() {
-  let client_tcp = TcpStream::connect("127.0.0.1:8080");
-}
+pub fn client_stream() {
+  
+  let mut client_tcp = TcpStream::connect("127.0.0.1:8080").unwrap();
+  let buf = [0; 10];
+  let client_msg = client_tcp.write("hello, tcp".as_bytes());}
--- scanner/src/main.rs
+++ scanner/src/main.rs
@@ -1,30 +1,62 @@
 use std::net::{TcpListener,TcpStream};
-use std::io::prelude::*;
+use std::io::{self, stdout, Read, Write};
+mod client;
+pub use client::client_stream;
 
-pub fn stream(site: TcpListener)  {
-    for _clients in site.incoming() { 
-    println!("Connection attempt");
-    match site.accept() {
-        Ok((socket, addr)) => 
-        println!("SERVER MSG- Client: {addr:?} Socket:{socket:?}"), 
-        Err(e) => println!("Socket error: {e:?}")
-    }
 
+pub fn server(mut stream: TcpStream) {
+    let mut buf = [0; 512];
+    let mut read_client = 0;
+    while read_client == 0 {
+        read_client = stream.read(&mut buf).unwrap();
+        println!("bytes sent {}", read_client);
+    
+    
     }
+    stdout().write(&buf[0..read_client]).unwrap();
+
 }
 
 
-pub fn client() {
-    let mut client_tcp = TcpStream::connect("127.0.0.1:8080");
-    for stream in client_tcp {
-        
-        println!("CLIENT MSG: {:?}", stream)
-        
-    }
+pub fn client_function() {
+    let mut client_msg = String::new();
+    let client_tcp = TcpStream::connect("127.0.0.1:8080");
+    let mut buf = [0; 512];
+    for mut stream in client_tcp {
+        stream.read(&mut buf);
+        let user_input = io::stdin().read_line(&mut client_msg).unwrap();
+        stream.write_all(&user_input.to_be_bytes());
+        println!("CLIENT MSG: {client_msg:?} stream: {stream:?}")
+    } 
 }
 
 fn main() {
     let site = TcpListener::bind("127.0.0.1:8080").unwrap();
-    client();
-    stream(site);
+    for stream in site.incoming() {
+        match stream {
+            Ok(stream) => {
+                client::client_stream();
+                server(stream);
+                
+            }
+            Err(_e) => println!("NO SOCKET BIND")
+
+        }
+    }
 }
+
+
+
+
+// pub fn client_stream() {
+  
+//     let client_tcp = TcpStream::connect("127.0.0.1:8080");
+//     let mut client_msg = b"MIC CHECK MIC CHECK 1 2";
+//     for mut stream in client_tcp {
+//       stream.write_all(client_msg);
+      
+  
+//     }
+  
+  
+//   }
